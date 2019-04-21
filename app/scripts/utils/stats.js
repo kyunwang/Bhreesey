a685e395011518de67b1c0fb5a0652dab9c7face
Kangyun Wang <kevinwang8011@hotmail.com>
2019-04-21 13:36:10 +0200

    Add dat.gui and update three

--- app/scripts/utils/stats.js
+++ app/scripts/utils/stats.js
@@ -1,9 +1,11 @@
 import Stats from 'stats.js';
 
-export function createStats(panelNumber = 0) {
+export function createStats(type = 0) {
 	const stats = new Stats();
+	const panelType =
+		typeof type !== 'undefined' && type && !isNaN(type) ? parseInt(type) : 0;
 
-	stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+	stats.showPanel(panelType); // 0: fps, 1: ms, 2: mb, 3+: custom
 	document.body.appendChild(stats.dom);
 
 	return stats;
