713554396a614b43554ad5c673c7dddcacdf807e
Kangyun Wang <kevinwang8011@hotmail.com>
2019-09-07 14:41:14 +0200

    Create simple helpers

---
+++ app/scripts/utils/helpers.js
@@ -0,0 +1,21 @@
+export const isArray = array => {
+	return Array.isArray(array);
+};
+
+export const bindEventListeners = events => {
+	if (!isArray(events)) return;
+
+	events.forEach(event => {
+		const { type, callback, target } = event;
+		target.addEventListener(type, callback);
+	});
+};
+
+export const unbindEventListeners = events => {
+	if (!isArray(events)) return;
+
+	events.forEach(event => {
+		const { type, callback, target } = event;
+		target.removeEventListener(type, callback);
+	});
+};
